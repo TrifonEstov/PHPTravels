a69303281ecac003356ea9bcaabb05dbd3da30a5
Trifon Estov <trifon@phyreapp.com>
2022-03-26 14:16:14 +0200

    Initial commit

---
+++ phptravels/src/test/java/phptravels/StepDefinitions.java
@@ -0,0 +1,92 @@
+package phptravels;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class StepDefinitions {
+
+    public WebDriver driver;
+
+    @Before
+    public void setupDriver() {
+        System.setProperty("webdriver.chrome.driver", "/home/trifon/Desktop/QaiWare/PHPTravels/drivers/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void closeDriver(){
+        driver.quit();
+    }
+
+
+    @Given("the user is on PHP Travels homepage")
+    public void theUserIsOnPHPTravelsHomepage() {
+        driver.get("https://www.phptravels.net/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String homePageTitle = driver.getTitle();
+        Assert.assertEquals(homePageTitle, "PHPTRAVELS - PHPTRAVELS");
+    }
+
+    @When("he navigates to the sign in page")
+    public void heNavigatesToTheSignInPage() {
+        WebElement signInButton = driver.findElement(By.xpath("//*[@id=\"fadein\"]/header/div[1]/div/div/div[2]/div/div/a[2]"));
+        signInButton.click();
+        String loginPageTitle = driver.getTitle();
+        Assert.assertEquals(loginPageTitle, "Login - PHPTRAVELS");
+    }
+
+    @And("he enters {string} as account email")
+    public void heEntersAsAccountEmail(String email) {
+        WebElement emailField = driver.findElement(By.xpath("//*[@id=\"fadein\"]/div[1]/div/div[2]/div[2]/div/form/div[1]/div/input"));
+        emailField.sendKeys(email);
+    }
+
+    @And("he enters {string} as a password")
+    public void heEntersAsAPassword(String password) {
+        WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"fadein\"]/div[1]/div/div[2]/div[2]/div/form/div[2]/div[1]/input"));
+        passwordField.sendKeys(password);
+    }
+
+    @And("he clicks Login button")
+    public void heClicksLoginButton() {
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"fadein\"]/div[1]/div/div[2]/div[2]/div/form/div[3]/button"));
+        loginButton.click();
+    }
+
+    @Then("ensure that the account is successfully logged in")
+    public void ensureThatTheAccountIsSuccessfullyLoggedIn() {
+        String userDashboardTitle = driver.getTitle();
+        Assert.assertEquals(userDashboardTitle, "Dashboard - PHPTRAVELS");
+    }
+
+    @Then("ensure that the login is unsuccessful")
+    public void ensureThatTheLoginIsUnsuccessful() {
+        String loginPageTitle = driver.getTitle();
+        Assert.assertEquals(loginPageTitle, "Login - PHPTRAVELS");
+    }
+
+    @And("he clicks on Logout button")
+    public void heClicksOnLogoutButton() {
+        WebElement logOutButton = driver.findElement(By.xpath("//*[@id=\"fadein\"]/div[1]/div/div[3]/ul/li[5]/a"));
+        logOutButton.click();
+    }
+
+    @Then("ensure that the account is successfully logged out")
+    public void ensureThatTheAccountIsSuccessfullyLoggedOut() {
+        String loginPageTitle = driver.getTitle();
+        Assert.assertEquals(loginPageTitle, "Login - PHPTRAVELS");
+    }
+}
---
+++ phptravels/target/test-classes/phptravels/php_travels_tests.feature
@@ -0,0 +1,20 @@
+Feature: Account management
+  As a customer,
+  I want to be able to log into my account
+
+  Scenario: Successful login and logout
+    Given the user is on PHP Travels homepage
+    When he navigates to the sign in page
+    And he enters "user@phptravels.com" as account email
+    And he enters "demouser" as a password
+    And he clicks Login button
+    Then ensure that the account is successfully logged in
+    And he clicks on Logout button
+    Then ensure that the account is successfully logged out
+
+  Scenario: Unsuccessful sign in
+    Given the user is on PHP Travels homepage
+    When he navigates to the sign in page
+    And he enters "notexistinguser@gmail.com" as account email
+    And he enters "123456" as a password
+    Then ensure that the login is unsuccessful
